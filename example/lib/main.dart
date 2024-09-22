ab38bc8ba279f85e56680d415bc916cd2119741b
shajed99-Polygon <shajedur@polygontech.xyz>
2024-09-22 18:13:48 +0600

    Done

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:on_process_button_widget/on_process_button_widget.dart';
 import 'package:on_text_input_widget/on_text_input_widget.dart';
+
+final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -19,17 +24,68 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Center(
-              child: OnTextInputWidget(
-            
-                minLines: 10,
-                maxLines: 9,
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 48),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    OnTextInputWidget(
+                      prefixIcon: Icon(Icons.add_box),
+                      showPrefixLoadingIcon: true,
+                      boxConstraints: BoxConstraints(minHeight: 48),
+                      fillColor: Colors.amber,
+                      isDense: false,
+                      isCollapsed: true,
+                      textAlignVertical: TextAlignVertical(y: 0),
+                      onChangedProcessing: (value) async {
+                        await Future.delayed(Duration(seconds: 2));
+
+                        print("0000 ___ $value");
+                      },
+                    ),
+                    ______Space(),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: OnTextInputWidget(
+                            hintText: "Hello",
+                            // showDetailError: true,
+                            validator: (value) {
+                              return "Press IT";
+                            },
+                          ),
+                        ),
+                        ______Space(),
+                        Flexible(
+                          child: OnProcessButtonWidget(
+                            onDone: (_) {
+                              _formKey.currentState?.validate();
+                            },
+                            child: Text("Okay"),
+                          ),
+                        ),
+                      ],
+                    ),
+                    ______Space(),
+                    Container(height: 24, color: Colors.amber)
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class ______Space extends StatelessWidget {
+  const ______Space();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: 8, width: 8);
   }
 }
--- lib/src/on_text_input_widget.dart
+++ lib/src/on_text_input_widget.dart
@@ -9,6 +9,7 @@ class OnTextInputWidget extends StatefulWidget {
     this.textEditingController,
     this.hintText = "",
     this.obscureText = false,
+    this.obscuringCharacter = "•",
     this.maxLines,
     this.minLines,
     this.maxLength,
@@ -21,14 +22,14 @@ class OnTextInputWidget extends StatefulWidget {
     this.onComplete,
     this.onTapOutside,
     this.onChanged,
-    this.onChangeDebouncer = const Duration(milliseconds: 500),
+    this.onChangeDebouncer = const Duration(milliseconds: 1000),
     this.onChangedProcessing,
     this.errorCheck,
     this.fullTextSelection = false,
     this.onTap,
     this.validator,
     this.errorBuilder,
-    this.showDetailError = true,
+    this.showDetailError = false,
     this.animationDuration = const Duration(milliseconds: 500),
     this.loadingIcon,
     this.loadingIconsSize = 24,
@@ -38,6 +39,36 @@ class OnTextInputWidget extends StatefulWidget {
     this.textAlign = TextAlign.start,
     this.errorTextStyle,
     this.errorColor,
+    this.keyboardType = TextInputType.text,
+    this.autofocus = false,
+    this.autocorrect = true,
+    this.enabled = true,
+    this.readOnly = false,
+    this.cursorColor,
+    this.autofillHints,
+    this.style,
+    this.isDense = true,
+    this.isCollapsed = false,
+    this.label,
+    this.labelText,
+    this.labelStyle,
+    this.floatingLabelBehavior,
+    this.prefix,
+    this.suffix,
+    this.showPrefixLoadingIcon = false,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.showSuffixLoadingIcon = false,
+    this.fillColor,
+    this.hintStyle,
+    this.errorStyle,
+    this.enabledBorder,
+    this.focusedBorder,
+    this.errorBorder,
+    this.focusedErrorBorder,
+    this.disabledBorder,
+    this.animationCurve = Curves.linear,
+    this.clipBehavior = Clip.antiAlias,
   }) : super(key: key);
 
   /// A controller for an editable text field.
@@ -61,6 +92,9 @@ class OnTextInputWidget extends StatefulWidget {
   /// Obscure Text Default obscureText: false
   final bool obscureText;
 
+  /// Obscuring character. Default: •
+  final String obscuringCharacter;
+
   /// Max Number of line
   final int? maxLines;
 
@@ -72,6 +106,12 @@ class OnTextInputWidget extends StatefulWidget {
 
   /// Animation Duration. Default: Duration(milliseconds: 500
   final Duration animationDuration;
+
+  /// Animation Curve. Default: Curves.linear
+  final Curve animationCurve;
+
+  /// Animation clip behavior. Default: Clip.antiAlias
+  final Clip clipBehavior;
 
   /// The vertical alignment of text within an input box.
   ///
@@ -91,7 +131,7 @@ class OnTextInputWidget extends StatefulWidget {
   /// Only supports text keyboards, other keyboard types will ignore this configuration. Capitalization is locale-aware.
   final TextCapitalization textCapitalization;
 
-  /// OnChangedProcessing function waiting duration. Default: Duration(milliseconds: 500)
+  /// OnChangedProcessing function waiting duration. Default: Duration(milliseconds: 1000)
   final Duration onChangeDebouncer;
 
   /// Select full text inside the input field when it is clicked. Default: false
@@ -124,6 +164,90 @@ class OnTextInputWidget extends StatefulWidget {
   /// Error text color. Default: Theme.of(context).colorScheme.error
   final Color? errorColor;
 
+  /// Keyboard Type of the text input. Default: TextInputType.text
+  final TextInputType keyboardType;
+
+  /// If true it will automatically open virtual keyboard. Default: false
+  final bool autofocus;
+
+  /// If true, it will show suggestion. Default: true
+  final bool autocorrect;
+
+  /// Enable or disable the textfield. Default: true
+  final bool enabled;
+
+  /// If true, the textfield is only readable. Default: false
+  final bool readOnly;
+
+  /// Cursor color of the text field. Default: Theme.of(context).colorScheme.primary
+  final Color? cursorColor;
+
+  /// Auto fill hint texts.
+  final List<String>? autofillHints;
+
+  /// Text style. Default: Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
+  final TextStyle? style;
+
+  /// Whether the [InputDecorator.child] is part of a dense form (i.e., uses less vertical space). Default: True
+  final bool isDense;
+
+  /// Whether the decoration is the same size as the input field. A collapsed decoration cannot have [labelText], [errorText], an [icon]. To create a collapsed input decoration, use [InputDecoration.collapsed].
+  final bool isCollapsed;
+
+  /// Optional widget that describes the input field.
+  final Widget? label;
+
+  /// Optional text that describes the input field.
+  final String? labelText;
+
+  /// The style to use for [InputDecoration.labelText] when the label is on top of the input field.  If null, the default style is used.
+  final TextStyle? labelStyle;
+
+  /// Defines how the floating label should behave.
+  final FloatingLabelBehavior? floatingLabelBehavior;
+
+  /// Optional widget to place on the line before the input.
+  final Widget? prefix;
+
+  /// Optional widget to place on the line after the input.
+  final Widget? suffix;
+
+  /// Prefix/Before Icon
+  final Widget? prefixIcon;
+
+  /// Suffix/After Icon
+  final Widget? suffixIcon;
+
+  /// If true, the loading Icon will be shown on the left side of the text field. Default: false
+  final bool showPrefixLoadingIcon;
+
+  /// If true, the loading Icon will be shown on the right side of the text field. Default: false
+  final bool showSuffixLoadingIcon;
+
+  /// Text field fill color. Default: widget.fillColor == null ? Theme.of(context).inputDecorationTheme.filled : true
+  final Color? fillColor;
+
+  /// Hint text style. Default: Theme.of(context).colorScheme.primary.withOpacity(0.5)
+  final TextStyle? hintStyle;
+
+  /// Error text style. Default: widget.hintStyle?.copyWith(color: Theme.of(context).colorScheme.error) ?? TextStyle(color: Theme.of(context).colorScheme.error)
+  final TextStyle? errorStyle;
+
+  /// Border style when enabled
+  final InputBorder? enabledBorder;
+
+  /// Border style when focused
+  final InputBorder? focusedBorder;
+
+  /// Border style when error occurred
+  final InputBorder? errorBorder;
+
+  /// Border style when error occurred and focused
+  final InputBorder? focusedErrorBorder;
+
+  /// Border style when disabled
+  final InputBorder? disabledBorder;
+
   //! Functions
   /// Function runs when the focused is change. It will be called when the text field is focused or unfocused.
   final Function(bool isFocused)? onFocusChange;
@@ -134,7 +258,7 @@ class OnTextInputWidget extends StatefulWidget {
   /// Function runs when pressing outside of the text field
   final void Function(PointerDownEvent pointerDownEvent)? onTapOutside;
 
-  ///
+  /// Creates a [FormField] that contains a [TextField].
   final String? Function(String? value)? validator;
 
   /// Function runs when the text field is tapped
@@ -258,14 +382,16 @@ class _OnTextInputWidgetState extends State<OnTextInputWidget> {
   }
 
   void unfocusKeyboard() {
-    if (focusNode == FocusManager.instance.primaryFocus?.offset) {
-      FocusManager.instance.primaryFocus?.unfocus();
-      FocusManager.instance.rootScope.unfocus();
-      FocusScope.of(context).unfocus();
-      focusNode = null;
-    } else {
-      focusNode = FocusManager.instance.primaryFocus?.offset;
-    }
+    try {
+      if (focusNode == FocusManager.instance.primaryFocus?.offset) {
+        FocusManager.instance.primaryFocus?.unfocus();
+        FocusManager.instance.rootScope.unfocus();
+        FocusScope.of(context).unfocus();
+        focusNode = null;
+      } else {
+        focusNode = FocusManager.instance.primaryFocus?.offset;
+      }
+    } catch (_) {}
 
     // FocusManager.instance.primaryFocus?.unfocus();
     // FocusManager.instance.rootScope.unfocus();
@@ -294,8 +420,17 @@ class _OnTextInputWidgetState extends State<OnTextInputWidget> {
           child: TextFormField(
             controller: textEditingController,
             obscureText: widget.obscureText,
+            obscuringCharacter: widget.obscuringCharacter,
             focusNode: widget.focusNode,
             textCapitalization: widget.textCapitalization,
+            keyboardType: widget.keyboardType,
+            autofocus: widget.autofocus,
+            autocorrect: widget.autocorrect,
+            enabled: widget.enabled,
+            readOnly: widget.readOnly,
+            cursorColor: widget.cursorColor ?? Theme.of(context).colorScheme.primary,
+            autofillHints: widget.autofillHints,
+            style: widget.style ?? Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
 
             // -------------------------------------------------------------------------------------
             maxLines: widget.obscureText
@@ -308,6 +443,7 @@ class _OnTextInputWidgetState extends State<OnTextInputWidget> {
 
             // -------------------------------------------------------------------------------------
             textAlignVertical: widget.textAlignVertical,
+            textAlign: widget.textAlign,
 
             //! Functions --------------------------------------------------------------------------
             onEditingComplete: () {
@@ -342,11 +478,11 @@ class _OnTextInputWidgetState extends State<OnTextInputWidget> {
               if (error) {
                 hintText = widget.hintText;
                 error = false;
-                if (widget.errorCheck != null) widget.errorCheck!(error, "");
               }
               if (mounted) setState(() {});
             },
             onTap: () {
+              // focusNode = FocusManager.instance.primaryFocus?.offset;
               if (widget.fullTextSelection && !firstTimeTap) {
                 textEditingController.selection = TextSelection(
                   baseOffset: 0,
@@ -381,44 +517,118 @@ class _OnTextInputWidgetState extends State<OnTextInputWidget> {
               if (mounted) setState(() {});
               return "";
             },
+            decoration: InputDecoration(
+              isDense: widget.isDense,
+              isCollapsed: widget.isCollapsed,
+              hintText: hintText.isEmpty ? null : hintText,
+              label: widget.label,
+              labelText: widget.labelText,
+              labelStyle: widget.labelStyle,
+              floatingLabelBehavior: widget.floatingLabelBehavior,
+              constraints: widget.boxConstraints ?? BoxConstraints(minHeight: height),
+              prefix: widget.prefix,
+              suffix: widget.suffix,
+              suffixIconConstraints: const BoxConstraints(),
+              prefixIconConstraints: const BoxConstraints(),
+              prefixIcon: SizedBox(
+                height: height,
+                child: _CustomAnimatedSize(
+                  alignment: Alignment.centerLeft,
+                  duration: widget.animationDuration,
+                  curve: widget.animationCurve,
+                  clipBehavior: widget.clipBehavior,
+                  child: widgetReplacement(showLoadingIcon(widget.showPrefixLoadingIcon) ?? _setIcon(widget.prefixIcon), contentPadding),
+                ),
+              ),
+              suffixIcon: SizedBox(
+                height: height,
+                child: _CustomAnimatedSize(
+                  alignment: Alignment.centerRight,
+                  duration: widget.animationDuration,
+                  curve: widget.animationCurve,
+                  clipBehavior: widget.clipBehavior,
+                  child: widgetReplacement(showLoadingIcon(widget.showSuffixLoadingIcon) ?? _setIcon(widget.suffixIcon), contentPadding),
+                ),
+              ),
+              filled: widget.fillColor == null ? Theme.of(context).inputDecorationTheme.filled : true,
+              fillColor: widget.fillColor ?? Theme.of(context).inputDecorationTheme.fillColor,
+              hintStyle: !error ? widget.hintStyle ?? TextStyle(color: Theme.of(context).colorScheme.primary.withOpacity(0.5)) : widget.errorStyle ?? widget.hintStyle?.copyWith(color: Theme.of(context).colorScheme.error) ?? TextStyle(color: Theme.of(context).colorScheme.error),
+              errorStyle: const TextStyle(height: -1),
+              contentPadding: contentPadding,
+              enabledBorder: widget.enabledBorder?.copyWith(borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.primary.withOpacity(0.5))) ??
+                  Theme.of(context).inputDecorationTheme.enabledBorder ??
+                  OutlineInputBorder(
+                    borderRadius: borderRadius,
+                    borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.primary.withOpacity(0.5)),
+                  ),
+              focusedBorder: widget.focusedBorder?.copyWith(borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.primary)) ??
+                  Theme.of(context).inputDecorationTheme.focusedBorder ??
+                  OutlineInputBorder(
+                    borderRadius: borderRadius,
+                    borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.primary),
+                  ),
+              errorBorder: widget.errorBorder?.copyWith(borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.error)) ??
+                  Theme.of(context).inputDecorationTheme.errorBorder ??
+                  OutlineInputBorder(
+                    borderRadius: borderRadius,
+                    borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.error),
+                  ),
+              focusedErrorBorder: widget.focusedErrorBorder?.copyWith(borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.primary.withOpacity(0.5))) ??
+                  Theme.of(context).inputDecorationTheme.focusedBorder ??
+                  OutlineInputBorder(
+                    borderRadius: borderRadius,
+                    borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.primary.withOpacity(0.5)),
+                  ),
+              disabledBorder: widget.disabledBorder?.copyWith(borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.primary.withOpacity(0.1))) ??
+                  Theme.of(context).inputDecorationTheme.disabledBorder ??
+                  OutlineInputBorder(
+                    borderRadius: borderRadius,
+                    borderSide: BorderSide(width: 2, color: Theme.of(context).colorScheme.primary.withOpacity(0.1)),
+                  ),
+            ),
           ),
+        ),
+        _CustomAnimatedSize(
+          alignment: Alignment.topCenter,
+          widthFactor: 1,
+          duration: widget.animationDuration,
+          curve: widget.animationCurve,
+          clipBehavior: widget.clipBehavior,
+          child: widget.errorBuilder != null ? widget.errorBuilder!(message ?? "") : errorChild(),
         ),
       ],
     );
   }
 }
 
-// class CustomAnimatedSize extends StatelessWidget {
-//   const CustomAnimatedSize({
-//     super.key,
-//     this.child,
-//     this.alignment = Alignment.center,
-//     required this.duration,
-//     this.curve = Curves.linear,
-//     this.clipBehavior = Clip.antiAlias,
-//     this.widthFactor,
-//     this.heightFactor,
-//   });
-//   final Widget? child;
-//   final AlignmentGeometry alignment;
-//   final Duration duration;
-//   final Curve curve;
-//   final Clip clipBehavior;
-//   final double? widthFactor;
-//   final double? heightFactor;
+class _CustomAnimatedSize extends StatelessWidget {
+  const _CustomAnimatedSize({
+    Key? key,
+    this.child,
+    required this.alignment,
+    required this.duration,
+    required this.curve,
+    required this.clipBehavior,
+    this.widthFactor,
+  }) : super(key: key);
+  final Widget? child;
+  final AlignmentGeometry alignment;
+  final Duration duration;
+  final Curve curve;
+  final Clip clipBehavior;
+  final double? widthFactor;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return FractionallySizedBox(
-//       widthFactor: widthFactor,
-//       heightFactor: heightFactor,
-//       child: AnimatedSize(
-//         curve: Curves.easeInOut,
-//         clipBehavior: clipBehavior,
-//         alignment: alignment,
-//         duration: duration,
-//         child: child ?? const SizedBox(),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: widthFactor,
+      child: AnimatedSize(
+        curve: Curves.easeInOut,
+        clipBehavior: clipBehavior,
+        alignment: alignment,
+        duration: duration,
+        child: child ?? const SizedBox(),
+      ),
+    );
+  }
+}
